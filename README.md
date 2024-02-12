cc3256df1a96f828fb44ab5a1db57586e1b2f753
Hassan Gamal <hgamal@momrah.gov.sa>
2024-02-12 12:42:34 +0300

    init backend

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # eazy-bank
+eazy bank is and web based banking application 
+developed using (springboot - angular - musql)
---
+++ back-end/ezy-back-api/src/main/java/com/eazybank/controller/ContactUsController.java
@@ -0,0 +1,13 @@
+package com.eazybank.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ContactUsController {
+
+    @GetMapping("contact")
+    public String contactUs(){
+        return "contact us";
+    }
+}
