d2ac93cd080b20108052201145d38c4e9b0efbb5
Hassan Gamal <hgamal@momrah.gov.sa>
2024-02-15 18:18:59 +0300

    add EazyBankAuthenticationProvider

--- back-end/eazy-bank-api/src/main/java/com/eazybank/security/EazyBankUserDetails.java
+++ back-end/eazy-bank-api/src/main/java/com/eazybank/security/EazyBankUserDetails.java
@@ -10,7 +10,7 @@ import java.util.Arrays;
 import java.util.Collection;
 
 @AllArgsConstructor
-public class CustomUserDetails implements UserDetails {
+public class EazyBankUserDetails implements UserDetails {
 
     private final Client client;
 
--- back-end/eazy-bank-api/src/main/java/com/eazybank/security/EazyBankUserDetailsService.java
+++ back-end/eazy-bank-api/src/main/java/com/eazybank/security/EazyBankUserDetailsService.java
@@ -10,13 +10,14 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class CustomUserDetailsService implements UserDetailsService {
+public class EazyBankUserDetailsService implements UserDetailsService {
 
     private final ClientRepository clientRepository;
 
+    // not used because I used the filter
     @Override
     public UserDetails loadUserByUsername(String username) {
-        return clientRepository.findByEmail(username).map(CustomUserDetails::new)
+        return clientRepository.findByEmail(username).map(EazyBankUserDetails::new)
                 .orElseThrow(() -> new UsernameNotFoundException("user '%s' not found".formatted(username)));
     }
 }
